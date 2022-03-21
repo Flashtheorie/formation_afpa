a59f51c4ee2d0335d47fceeb2fe3cdb717d5f01e
FLashtheorie <antoinepascualmail@gmail.com>
2022-03-21 10:38:52 +0100

    1er exercice fait

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,6 @@
-var a = 
+
+var cote_carre  = prompt("Quel est le coté du carré ?"); 
+
+var aire = cote_carre * cote_carre;
+
+document.write("<h1>L'aire du carré avec " + cote_carre + "cm de coté est de " + aire + "cm²</h1>");
