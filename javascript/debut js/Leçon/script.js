2c65a26acc363711ff961d055e1eb9d225ba27b8
FLashtheorie <antoinepascualmail@gmail.com>
2022-03-22 09:23:13 +0100

    Début page 1

--- javascript/debut js/Leçon/script.js
+++ javascript/debut js/Leçon/script.js
@@ -1,1 +1,12 @@
-document.write('<h1>Coucou<h1/>')
+let age = 17;
+let majorite = 18;
+
+if (age >= majorite) {
+	document.write("Vous pouvez passer le permis");
+}
+else if (age < majorite && age >= 16) {
+	document.write("Vous pouvez passer la conduite accompagnée")
+}
+else {
+	document.write("Vous êtes un enfant")
+}
